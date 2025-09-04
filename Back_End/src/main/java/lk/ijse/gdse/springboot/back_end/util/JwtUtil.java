dd43d6a5748600bf5f346c3a35e1d9ea35b931bb
ThamiduChamod <thamiduchamod748@gmail.com>
2025-09-04 22:55:41 +0530

    create jwt token

---
+++ Back_End/src/main/java/lk/ijse/gdse/springboot/back_end/util/JwtUtil.java
@@ -0,0 +1,31 @@
+package lk.ijse.gdse.springboot.back_end.util;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("866000000")
+    private String expiration;
+
+    @Value("asdddddfegrgtyjuyluiotyiuytttjmtygfhnsrtyujy7iuyukukgshgrty5yjytgkj8ioftefqw214erfecdsdrgh")
+    private String secretKey;
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(
+                        System.currentTimeMillis() + expiration))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes())
+                        , SignatureAlgorithm.HS256).compact();
+    }
+
+
+}
