71e4d9e8a4350f56b955f5b697d81860c736ab23
ThamiduChamod <thamiduchamod748@gmail.com>
2025-09-11 00:46:15 +0530

    create company repository

---
+++ Back_End/src/main/java/lk/ijse/gdse/springboot/back_end/repository/CompanyProfile.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.springboot.back_end.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyProfile extends JpaRepository<CompanyProfile, Long> {
+
+
+}
