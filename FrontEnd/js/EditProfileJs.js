6182ec861ba50977a0101926e1810480af03126c
ThamiduChamod <thamiduchamod748@gmail.com>
2025-09-12 11:47:23 +0530

    add map for update profile

---
+++ FrontEnd/js/EditProfileJs.js
@@ -0,0 +1,16 @@
+const saveBtn = $('#saveBtn');
+const cancelBtn = $('#cancelBtn');
+// Profile Image
+const uploadProfilePhoto = $('#uploadProfilePhoto');
+const uploadBannerImage = $("#uploadBannerImage");
+//Basic Information
+const companyName = $('#companyName');
+const companyTagline = $('#companyTagline');
+const companyIndustry = $('#companyIndustry');
+// Company Overview
+const companyOverview = $('#companyOverview');
+const companyMission = $('#companyMission');
+const companyVision = $('#companyVision');
+const companyLocations = $('#companyLocations');
+
+
